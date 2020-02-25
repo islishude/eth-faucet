c7c636fa52e34bb68880690899eea67c45a50968
Shude Li <islishude@gmail.com>
2020-02-25 12:20:58 +0800

    Update main.go

--- main.go
+++ main.go
@@ -12,27 +12,24 @@ func checkEthAddress(address string) bool {
 	return regexp.MustCompile(`^0x[0-9a-fA-F]{40}$`).MatchString(address)
 }
 
+const api = "https://faucet.ropsten.be/donate/"
+
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Println(`ropsten eth faucet
-		example:
-		
-		$ eth-faucet 0x0581a31Bc9d1c030B004951D8bC520A07fcb3897`)
+		fmt.Println(`ropsten ethereum faucet
+
+example:
+
+$ eth-faucet 0x0581a31Bc9d1c030B004951D8bC520A07fcb3897`)
 		return
 	}
 
 	address := os.Args[1]
-	if address == "" {
-		fmt.Println("address is empty")
-		return
-	}
-
 	if !checkEthAddress(address) {
 		fmt.Println("Invalid address")
 		return
 	}
 
-	const api = "https://faucet.ropsten.be/donate/"
 	resp, err := http.Get(api + address)
 	if err != nil {
 		fmt.Println("Request error", err)
